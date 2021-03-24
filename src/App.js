412c6c72873e43b3096239725840e2746bd94e37
toprakkoc <hello@toprak.dev>
2021-03-25 00:44:47 +0300

    router refactor

--- src/App.js
+++ src/App.js
@@ -1,42 +1,32 @@
 import React from "react";
+
 import SignIn from "./components/Login/SignIn";
 import SignUp from "./components/Login/SignUp";
 import Dashboard from "./components/Dashboard";
+
 import AOS from "aos";
 import "aos/dist/aos.css";
+
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 import 'alertifyjs/build/css/alertify.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 /*Animation Library Initialization */
 AOS.init({
   once: true
 });
 
-
-function App() {
-
+const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Switch>
-        <Route exact path="/">
-            <SignIn />
-          </Route>
-
-          <Route path="/sign-up">
-            <SignUp />
-          </Route>
-
-          <Route path="*">
-            <Dashboard/>
-          </Route>
-
-
-        </Switch>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" children={<SignIn />} />
+        <Route path="/sign-up" children={<SignUp />} />            
+        <Route path="*" children={<Dashboard/>} />
+      </Switch>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
